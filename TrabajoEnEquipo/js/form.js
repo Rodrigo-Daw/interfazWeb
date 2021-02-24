16e275e04644ef7dbbfa1f19d93303e4cbebd2a5
ammDAW <albertom.martinez@hotmail.com>
2021-02-24 12:08:48 +0100

    pequeñas mejoras

--- TrabajoEnEquipo/js/form.js
+++ TrabajoEnEquipo/js/form.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
 	const btnSubmit = document.querySelector("#btnSubmit");
-    const email = document.querySelector("#email");
-    const nombre = document.querySelector("#nombre");
-    const apellidos = document.querySelector("#apellidos");
-    const consentimiento = document.querySelector("#consentimiento");
-
-	btnSubmit.addEventListener("click", enviarAlerta(email, nombre, apellidos, consentimiento));
+	btnSubmit.addEventListener("click", enviarAlerta);
 })
 
-function enviarAlerta(email, nombre, apellidos, consentimiento){
-    if(email != "" && nombre != "" && apellidos != "" && consentimiento!= "")
+/* function enviarAlerta(){
+    const email = document.querySelector("#email").value;
+    const nombre = document.querySelector("#nombre").value;
+    const apellidos = document.querySelector("#apellidos").value;
+    const consentimiento = document.querySelector("#consentimiento").value;
+    if(email != null && nombre != null && apellidos != null && consentimiento!= null)
         alert("Formulario enviado. Gracias por suscribirse");
+} */
+
+function enviarAlerta(){
+    alert("Formulario enviado. Gracias por suscribirse");   
 }
+    
 
