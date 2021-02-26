0289afabe8138470b8ef214272fcb57794633903
Alberto Martinez <albertom.martinez@hotmail.com>
2021-02-26 09:10:48 +0100

    js terminado

--- TrabajoEnEquipo/js/form.js
+++ TrabajoEnEquipo/js/form.js
@@ -1,19 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
-	const btnSubmit = document.querySelector("#btnSubmit");
-	btnSubmit.addEventListener("click", enviarAlerta);
-})
+$("#formBoletin").submit(function(evento){
+    evento.preventDefault();
+    $("#nombre").val("");
+    $("#apellidos").val("");
+    $("#email").val("");
+    $("#consentimiento").prop("checked", false);
 
-/* function enviarAlerta(){
-    const email = document.querySelector("#email").value;
-    const nombre = document.querySelector("#nombre").value;
-    const apellidos = document.querySelector("#apellidos").value;
-    const consentimiento = document.querySelector("#consentimiento").value;
-    if(email != null && nombre != null && apellidos != null && consentimiento!= null)
-        alert("Formulario enviado. Gracias por suscribirse");
-} */
+    alert ("Formulario enviado. Gracias por suscribirse");
+});
 
-function enviarAlerta(){
-    alert("Formulario enviado. Gracias por suscribirse");   
-}
-    
 
